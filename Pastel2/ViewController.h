d7e86a5a5b0a2c07d32a5796127d08262d99a4e2
sscnt <pixeltc@gmail.com>
2014-05-27 17:33:58 +0900

    Initial Commit

---
+++ Pastel2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Pastel2
+//
+//  Created by SSC on 2014/05/27.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
