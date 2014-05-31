8ddb961908ab50f0ccdf7205099dee25e9befead
sscnt <pixeltc@gmail.com>
2014-06-01 02:57:02 +0900

    a

---
+++ Pastel2/PtFtViewLayerStrengthSlider.h
@@ -0,0 +1,43 @@
+//
+//  VnViewSlider.h
+//  Pastel
+//
+//  Created by SSC on 2014/05/11.
+//  Copyright (c) 2014年 SSC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "PtFtViewSlider.h"
+#import "VnViewEditorLayerBarButton.h"
+
+@class PtFtViewLayerStrengthSlider;
+
+@protocol VnViewSliderDelegate
+- (void)slider:(PtFtViewLayerStrengthSlider*)slider DidValueChange:(CGFloat)value;
+- (void)sliderDidValueResetToDefault:(PtFtViewLayerStrengthSlider*)slider;
+- (BOOL)sliderShouldValueResetToDefault:(PtFtViewLayerStrengthSlider*)slider;
+- (void)touchesBeganWithSlider:(PtFtViewLayerStrengthSlider*)slider;
+- (void)touchesEndedWithSlider:(PtFtViewLayerStrengthSlider*)slider;
+@end
+
+@interface PtFtViewLayerStrengthSlider : UIView <UISliderViewDelegate>
+{
+    float _paddingLeft;
+}
+
+@property (nonatomic, strong) PtFtViewSlider* sliderView;
+@property (nonatomic, weak) VnViewEditorLayerBarButton* button;
+@property (nonatomic, assign) CGFloat value;
+@property (nonatomic, assign) CGFloat defaultValue;
+@property (nonatomic, assign) CGFloat alpha;
+@property (nonatomic, weak) id<VnViewSliderDelegate> delegate;
+@property (nonatomic, strong) NSString* title;
+@property (nonatomic, assign) SliderViewTitlePosition titlePosition;
+@property (nonatomic, assign) BOOL locked;
+@property (nonatomic, assign) VnEffectGroup effectGroup;
+
+@property (nonatomic, strong) UIColor* sliderStrokeColor;
+@property (nonatomic, strong) UIColor* sliderBgColor;
+@property (nonatomic, strong) UIColor* sliderThumbColor;
+
+@end
